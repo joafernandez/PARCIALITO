464b58f6901d0aac181eecf2aa5157421fe07f43
joafernandez <efernandez@fie.undef.edu.ar>
2023-03-31 10:36:48 -0300

    inicio programita

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    printf("ingrese una nota");
+    int num;
+    int cont=0;
+    int suma=0;
+    int promedio;
+    printf("ingrese una nota\n:");
+    scanf("%d",num);
+    while(num!=0)
+    {
+        cont++;
+        suma=suma+num;
+        promedio=suma%cont;}
+
+    printf("ingrese una nota\n:");
+    printf("el promedio es %d\n:",promedio );
+
     return 0;
 }
